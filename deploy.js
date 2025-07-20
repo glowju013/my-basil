9a2a423f6cbd1eccb003315c70ba74f7d8241c5d
glowju013 <76641389+glowju013@users.noreply.github.com>
2025-07-20 19:31:06 +0900

    fix: setting file

---
+++ deploy.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { execSync } = require('child_process');
+
+const name = process.env.GH_NAME;
+const email = process.env.GH_EMAIL;
+const token = process.env.GH_TOKEN;
+
+const repo = 'https://${token}@github.com/glowju013/my-basil.git';
+
+const command = `npx gh-pages -d build -u "${name} <${email}>" --repo ${repo}`;
+
+try {
+  console.log("🚀 Deploying to GitHub Pages...");
+  execSync(cmd, { stdio: 'inherit' });
+  console.log("✅ 배포 성공!");
+} catch (error) {
+  console.error("🚨 배포 실패:", error.message);
+  process.exit(1);
+}
